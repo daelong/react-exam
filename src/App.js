0d31d2fa785ceed0c1ed10650eb862899d36e6ee
DaeHyun Lee <dhlee2@autocrypt.io>
2022-10-30 22:19:04 +0900

    exam5 clear

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import Exam1 from "./components/Exam1/Exam1";
 import Exam2 from "./components/Exam2/Exam2";
 import Exam3 from "./components/Exam3/Exam3";
 import Exam4 from "./components/Exam4/Exam4";
+import Exam5 from "./components/Exam5/Exam5";
 
 function App() {
-  return <Exam4 />;
+  return <Exam5 />;
 }
 
 export default App;
